ac578941cb8e940e7d04ce57f56a74fa9d39183d
LearnUseZone <learnusezone@gmail.com>
2020-11-15 10:07:51 +0100

    refactor(workflowrsubdirs): Remove a variable in generate_rmd() style(workflowrsubdirs): Adjust comments and description in generate_rmd()

--- code/workflowrsubdirs/workflowrsubdirs/R/generate-rmd.R
+++ code/workflowrsubdirs/workflowrsubdirs/R/generate-rmd.R
@@ -10,7 +10,7 @@
 #' it will be not exported therefore input variables have no default values.
 #' @param orig_rmd_path
 #' character
-#' Path to an original .Rmd knitr file.
+#' Path to an original .Rmd file saved in workflowr subdirectory.
 #' @param temp_rmd_path
 #' character
 #' Name ("--" is usually part of it's name) of a temporary .Rmd file that will be temporarily saved into directory "analysis".
@@ -23,23 +23,20 @@
 #' }
 
 generate_rmd <- function(orig_rmd_path, temp_rmd_path) {
-  orig_rmd_rel_path <- base::file.path(".", orig_rmd_path)  # relative path to an original .Rmd file, "." is used for setting a correct path in parameter "child" of "r chunk" below
   base::cat(
     "---\n",
-    yaml::as.yaml(rmarkdown::yaml_front_matter(orig_rmd_rel_path)),  # YAML header from an original .Rmd file
+    # YAML header copied (except comments) from an original .Rmd file
+    yaml::as.yaml(rmarkdown::yaml_front_matter(orig_rmd_path)),
     "---\n\n",
-    "**Source file:** ", orig_rmd_path,  # link to original .Rmd file from workflowr subdirectory
+    "**Source file:** ", orig_rmd_path,
     "\n\n",
 
-    # r chunk code (not YAML header); [lit 4]
-    "```{r child = base::file.path(knitr::opts_knit$get(\"output.dir\"), \".", orig_rmd_rel_path, "\")}\n```",  # output.dir = directory "analysis" (in this case)
+    # r chunk code (not YAML header)
+    #   "output.dir" returns directory "analysis" in this case (also tested) and "../" goes one directory up
+    "```{r child = base::file.path(knitr::opts_knit$get(\"output.dir\"), \"../", orig_rmd_path, "\")}\n```",
+
     file = temp_rmd_path,  # file = a name of file that will be created
     sep = "",
     append = F
   )
-  # Notes
-  #   Explanation of usage dot defined by \"." in r child in r chunk code
-  #    - A dot defined by \"." is required because knitr::opts_knit$get(\"output.dir\") returns "analysis"
-  #      as output directory in this case so "child" parameter of "r chunk"
-  #      has to 1stly go one directory up (orig_rmd_rel_path starts with "./" + following \"." => ../)
 }
